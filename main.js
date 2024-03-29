9481cdfa618e990972e7fef04343c14e92cfa73b
RamilaKhalil <165261119+RamilaKhalil@users.noreply.github.com>
2024-03-29 17:01:31 +0500

    Add files via upload
    coding challenge one

---
+++ main.js
@@ -0,0 +1,4 @@
+var username = "ayesha";
+console.log(username);
+var messsage = "asalam o alaikum its me ayesha kese hen aap";
+console.log(messsage);
---
+++ main.ts
@@ -0,0 +1,4 @@
+let username = "ayesha";
+console.log(username);
+let messsage = "asalam o alaikum its me ayesha kese hen aap";
+console. log(messsage);
